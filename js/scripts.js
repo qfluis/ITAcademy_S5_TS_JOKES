e9c8cc15cf2d2958b56069ccbb8c4de0250d1dd9
luis Quevedo <qfluis@gmail.com>
2022-02-04 00:50:06 +0100

    Ejercicio 3 Finalizado

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,23 @@
 "use strict";
+const reportJokes = [];
 // Evento Click botón
 const btn = document.getElementById("btn_chiste");
 btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", cargarChiste);
+// Eventos valoración
+const btnValArray = document.querySelectorAll("#valoracion a img");
+for (let btnVal of btnValArray) {
+    btnVal.addEventListener("click", valorarChiste);
+}
+function valorarChiste() {
+    displayVotes("hidden");
+    const report = {
+        joke: document.getElementById("chiste").innerHTML,
+        score: parseInt(this.alt),
+        date: new Date().toISOString()
+    };
+    reportJokes.push(report);
+    console.log(reportJokes);
+}
 // Peticion del chiste a API
 function cargarChiste() {
     const API_URL = "https://icanhazdadjoke.com";
@@ -18,5 +34,18 @@ function cargarChiste() {
         .then((response) => response.json())
         .then((joke) => {
         HTMLResponse.innerHTML = joke.joke;
+        displayVotes("visible");
     });
+}
+/*
+function switchDisplayVotes(){
+    const visible = document.getElementById("valoracion")!.style.visibility;
+    let visibility:string = (visible == "visible")?"hidden":"visible";
+        
+    document.getElementById("valoracion")!.style.visibility = visibility;
+    (<HTMLElement>document.querySelector("#contenedor_chiste h3"))!.style.visibility = visibility;
+}*/
+function displayVotes(visibility) {
+    document.getElementById("valoracion").style.visibility = visibility;
+    document.querySelector("#contenedor_chiste h3").style.visibility = visibility;
 }
--- ts/scripts.ts
+++ ts/scripts.ts
@@ -1,11 +1,37 @@
+// BD ARRAY
+interface IreportJoke {
+    joke: string,
+    score: number,
+    date: string
+}
+
+const reportJokes: IreportJoke[] = [];
+
 
 // Evento Click botón
-const btn = document.getElementById("btn_chiste");
+const btn:HTMLElement | null = document.getElementById("btn_chiste");
 btn?.addEventListener("click",cargarChiste);
+
+// Eventos valoración
+const btnValArray = document.querySelectorAll("#valoracion a img");
+for (let btnVal of btnValArray) {
+    btnVal.addEventListener("click", valorarChiste);
+}
+
+function valorarChiste():void{    
+    displayVotes("hidden");
+    const report:IreportJoke = {
+        joke: document.getElementById("chiste")!.innerHTML,
+        score: parseInt(this.alt),
+        date: new Date().toISOString()
+    }
+    reportJokes.push(report);
+    console.log(reportJokes);
+}
 
 
 // Peticion del chiste a API
-function cargarChiste(){
+function cargarChiste():void{
     const API_URL: string = "https://icanhazdadjoke.com";
     //const API_URL: string = "https://api.chucknorris.io/jokes/random";
     const HTMLResponse:HTMLElement | null = document.getElementById("chiste");
@@ -22,7 +48,20 @@ function cargarChiste(){
         .then((response) => response.json())
         .then((joke)=>{
             HTMLResponse!.innerHTML = joke.joke;
+            displayVotes("visible");
         });
     
 
+}
+/*
+function switchDisplayVotes(){
+    const visible = document.getElementById("valoracion")!.style.visibility;
+    let visibility:string = (visible == "visible")?"hidden":"visible";
+        
+    document.getElementById("valoracion")!.style.visibility = visibility;
+    (<HTMLElement>document.querySelector("#contenedor_chiste h3"))!.style.visibility = visibility;
+}*/
+function displayVotes(visibility:string):void{
+    document.getElementById("valoracion")!.style.visibility = visibility;
+    (<HTMLElement>document.querySelector("#contenedor_chiste h3"))!.style.visibility = visibility;
 }
